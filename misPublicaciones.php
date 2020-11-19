278ce98aa5a854aa2183f38a926997ac6fccc5bf
rastyboys2 <73666371+rastyboys2@users.noreply.github.com>
2020-11-19 02:47:46 -0300

    Mis publicaciones en desarollo

--- misPublicaciones.php
+++ misPublicaciones.php
@@ -178,7 +178,7 @@ if(!isset($_SESSION['nombre'])){
 
     <!---Lista de Mis publicaciones-->
 
-    
+    <form class="p-4 p-md-5 border rounded" method="post" action="php/Publicar.php" method="POST">
     <section class="site-section">
       <div class="container">
         <div class="row mb-5 justify-content-center">
@@ -195,15 +195,15 @@ if(!isset($_SESSION['nombre'])){
         
         <ul class="job-listings mb-5">
         <?php
-          $query="SELECT titulo, usuario.nombre, comuna.nombreComuna, region.nombreRegion, tipotrabajo.nombreTipo FROM trabajo INNER JOIN usuario ON trabajo.idUsuario = usuario.idUsuario INNER JOIN tipotrabajo ON trabajo.idTipo = tipotrabajo.idTipo INNER JOIN direccion ON trabajo.idDireccion = direccion.idDireccion INNER JOIN comuna ON direccion.idComuna = comuna.idComuna INNER JOIN region ON comuna.idRegion = region.idRegion where trabajo.idUsuario = '$id'";
+          $query="SELECT idTrabajo,titulo, usuario.nombre, comuna.nombreComuna, region.nombreRegion, tipotrabajo.nombreTipo FROM trabajo INNER JOIN usuario ON trabajo.idUsuario = usuario.idUsuario INNER JOIN tipotrabajo ON trabajo.idTipo = tipotrabajo.idTipo INNER JOIN direccion ON trabajo.idDireccion = direccion.idDireccion INNER JOIN comuna ON direccion.idComuna = comuna.idComuna INNER JOIN region ON comuna.idRegion = region.idRegion where trabajo.idUsuario = '$id'";
           $resultado= $mysqli->query($query); 
           while($var=mysqli_fetch_row($resultado)){
           ?>
-          <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+         <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
             <?php
             if($mis == true){
               ?>
-              <a href="job-single.php"></a>
+              <a href="job-single.php?publicacion=<?php echo $var[0]?>"></a>
               <?php
             }else{
               ?>
@@ -218,19 +218,26 @@ if(!isset($_SESSION['nombre'])){
 
             <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
               <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
-                <h2><?php echo $var[0] ?></h2>
+                <h2><?php echo $var[1] ?></h2>
                                 
-                <strong><?php echo $var[1] ?></strong>
+                <strong><?php echo $var[2] ?></strong>
               </div>
               <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
-                <span class="icon-room"></span> <?php echo $var[2] ?>, <?php echo $var[3] ?>
+                <span class="icon-room"></span> <?php echo $var[3] ?>, <?php echo $var[4] ?>
+                <span class="badge badge-danger"><?php echo $var[5] ?></span>
               </div>
               <div class="job-listing-meta">
-                <span class="badge badge-danger"><?php echo $var[4] ?></span>
+              <button href=<?php echo $ref ?> class="btn btn-info border-width-2 d-none d-lg-inline-block">Editar</button>
               </div>
+              <div class="job-listing-meta">
+              <button href=<?php echo $ref ?> class="btn btn-danger border-width-2 d-none d-lg-inline-block">Eliminar</button>
+              </div>
+              
               <?php } ?>     
-            </div>             
-        </ul>
+            </div>
+          </form>             
+            </ul>
+
 <!-- Mostrar cantidad de trabajos  -->
         <div class="row pagination-wrap">
           <div class="col-md-6 text-center text-md-left mb-4 mb-md-0">
